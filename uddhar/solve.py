fc69bc085ed88fea6baa209d99e2d2cc0a795c1f
Siddhant Chhabra <siddhant1042@gmail.com>
2019-05-09 23:59:41 +0530

    Added Traversal Order Calculations

--- uddhar/solve.py
+++ uddhar/solve.py
@@ -5,54 +5,73 @@ Modified Krushkull's Algo.
 
 """
 
+import queue
+
 def main():
-    edge_list = []
-    edges = open("edges.txt","r")
-    for e in edges:
-        edge = tuple(e.split())
-        edge_list.append(edge)
-    
-    #assuming 10 nodes for this poc
-    link = [i for i in range(11)]
-    size = [1 for i in range(11)]
-    
     solved=[]
-    
+    edge_list = open("edges.txt","r").readlines
+    link = [i for i in range(3)]
+    size = [1 for i in range(3)]
+
+    adj=[[] for i in range(3)]
+
     for e in edge_list:
         node_a = e[0]
         node_b = e[1]
         weight_ab = e[2]
-
-        if(!same(node_a,node_b)):
-            unite(node_a,node_b)
+        if(!same(node_a,node_b,link)):
+            unite(node_a,node_b,size,link)
+            adj[node_a].extend(node_b)
+            adj[node_b].extend(node_a)
             solved.append(e)
         else:
+            traversal_order = bfs(node_a,node_b,adj)
+            first = node_a
+            '''
+            Dumb solution
+            '''
+            for t in traversal_order:
+                for dumb in enumerate(solved):
+                    if first==dumb[1][0] & t = dumb[1][1]:
+                        solved[dumb[0]][2]+=weight_ab
+                    elif t==dumb[1][0] & first = dumb[1][1]:
+                        solved[dumb[0]][2]-=weight_ab
+                first = t
+    
+    print(solution)
 
+def bfs(start,destination,adj):
+    bfs_q = queue.Queue
+    processed = [False for i in range(3)]
+    traversal_order = []
+    q.put(start)
+    processed[start]=True
+    while(!q.empty()):
+        node = q.get()
+        traversal_order.append(node)
+        if node == destination:
+            return node
+        for neighbour in adj[node]:
+            if processed[neighbour]: 
+                continue
+            processed[node]=True
+            q.put(neighbour)
 
-def dfs(node):
-    if visited(node):
-        continue
-    visited[node]=True
-    for i in adj[node]:
-        djs(i)
-
-def find(node):
+def find(node,link):
     x = node
     while(x!=link[x]): 
         x = link[x]
     return x 
 
-def same(node_a,node_b):
-    if find(node_a)!=find(node_b):
+def same(node_a,node_b,link):
+    if find(node_a,link)!=find(node_b,link):
         return False
     return True 
 
-def unite(node_a,node_b):
+def unite(node_a,node_b,size,link):
     a = find(node_a)
     b = find(node_b)
     if (size[a]<size[b]):
         a,b=b,a
     size[a]+=size[b]
     link[b]=a
-
-    
