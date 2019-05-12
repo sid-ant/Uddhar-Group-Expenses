e8ce98f44ea06f9886079f1a925b26c552f53362
Siddhant Chhabra <siddhant1042@gmail.com>
2019-05-13 01:07:51 +0530

    Added Login using JWTs

--- uddhar/__init__.py
+++ uddhar/__init__.py
@@ -6,32 +6,26 @@ import logging
 
 def create_app(test_config=None):
 
-    logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # create and configure the app
+    logging.basicConfig(filename='app.log', filemode='a+', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     app = Flask(__name__, instance_relative_config=True)
+    # placeholder secret_key, overwritten by reading from config file -- not tracked in git 
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY="dev", 
         DATABASE=os.path.join(app.instance_path, 'uddhari.sqlite'),
     )
 
+    app.config.from_json('development.json')
+
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-    
     from . import db
     db.init_app(app)
 
--- uddhar/auth.py
+++ uddhar/auth.py
@@ -5,6 +5,9 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from uddhar.db import get_db
 
+import jwt 
+import datetime
+
 bp = Blueprint('auth',__name__,url_prefix='/auth')
 
 @bp.route('/register',methods=['POST'])
@@ -38,3 +41,39 @@ def register():
 
     responseObject['status']="registered,successfully"
     return make_response(jsonify(responseObject)),201
+
+@bp.route('/login',methods=['POST'])
+def login():
+    req =  request.get_json()
+    password =  req.get("password")
+    email = req.get("email")
+
+    responseObject = {
+        'status':None, 
+        'token':None
+    }
+
+    if email==None or password==None:
+        responseObject['status']="email and password required"
+        return make_response(jsonify(responseObject)),400
+    
+    db = get_db()
+
+    user =  db.execute('SELECT * FROM user WHERE email = ?', (email,)).fetchone()
+
+    if user is None or check_password_hash(user['password'],password) is False:
+        responseObject['status']="authentication failure"
+        return make_response(jsonify(responseObject)),401
+    
+    payload = {
+        'sub':user['user_id'],
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+        'iat': datetime.datetime.utcnow(),
+    }
+    current_app.logger.debug(current_app.config['SECRET_KEY'])
+    token = jwt.encode(payload,current_app.config['SECRET_KEY'],algorithm='HS256')
+    responseObject['status']="Success"
+    responseObject['token']=token.decode()
+
+    return make_response(jsonify(responseObject)),200
+
